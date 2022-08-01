a683b0e3a4dfcb1f703ce923780ebe031b35756b
Alexander Popov <as-popov@yandex.ru>
2022-08-01 22:22:54 +0300

    @Transactional на сервис

--- icepeak/src/main/java/aspopov/icepeak/warehouse/service/SkiServiceImpl.java
+++ icepeak/src/main/java/aspopov/icepeak/warehouse/service/SkiServiceImpl.java
@@ -3,6 +3,7 @@ package aspopov.icepeak.warehouse.service;
 import aspopov.icepeak.warehouse.projection.SkiModelGalleryProjection;
 import aspopov.icepeak.warehouse.repository.SkiRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +16,7 @@ public class SkiServiceImpl implements SkiService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<SkiModelGalleryProjection> findAllForGallery() {
         return skiRepository.findAllSkiForGallery();
     }
