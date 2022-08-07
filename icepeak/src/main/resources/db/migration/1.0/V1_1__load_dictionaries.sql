99c33250a946a3dbd11e3251ac79e143a0224efc
Alexander Popov <as-popov@yandex.ru>
2022-08-07 16:20:56 +0300

    Изменен справочник Gender

--- icepeak/src/main/resources/db/migration/1.0/V1_1__load_dictionaries.sql
+++ icepeak/src/main/resources/db/migration/1.0/V1_1__load_dictionaries.sql
@@ -1,7 +1,6 @@
 
-insert into d_gender(id_gender, gender) values (1, 'Мужские');
-insert into d_gender(id_gender, gender) values (2, 'Женские');
-insert into d_gender(id_gender, gender) values (3, 'Унисекс');
+insert into d_gender(id_gender, gender) values (2, 'Для женщин');
+insert into d_gender(id_gender, gender) values (3, 'Для мужчин и женщин');
 
 insert into d_age(id_age, age) values (1, 'Для взрослых');
 insert into d_age(id_age, age) values (2, 'Для ребенка');
