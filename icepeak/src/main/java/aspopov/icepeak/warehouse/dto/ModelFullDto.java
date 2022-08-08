8489cbccd403724a0f0d7051aa1afbd09b0cd9a9
Alexander Popov <as-popov@yandex.ru>
2022-08-08 15:25:00 +0300

    Добавлены поля Age и Gender в ModelFullDto

--- icepeak/src/main/java/aspopov/icepeak/warehouse/dto/ModelFullDto.java
+++ icepeak/src/main/java/aspopov/icepeak/warehouse/dto/ModelFullDto.java
@@ -5,28 +5,21 @@ import aspopov.icepeak.warehouse.domain.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ModelFullDto {
-    private long id;
-    private short category;
-    private String model;
+public class ModelFullDto extends ModelShortDto {
     private String description;
-    private String vendor;
-    private int price;
-    private byte[] image;
+    private String age;
+    private String gender;
     private List<ProductDto> products;
 
-    public ModelFullDto(long id, short category, String model, String description, String vendor, int price, byte[] image) {
-        this.id = id;
-        this.category = category;
-        this.model = model;
+    public ModelFullDto(long id, short category, String model, String description, String age, String gender, String vendor, int price, byte[] image) {
+        super(id, category, model, vendor, price, image);
         this.description = description;
-        this.vendor = vendor;
-        this.price = price;
-        this.image = image;
+        this.age = age;
+        this.gender = gender;
     }
 
     public static ModelFullDto fromDomain(Model model) {
-        var modelDto = new ModelFullDto(model.getId(), model.getCategory(), model.getName(), model.getDescription(), model.getVendor().getName(), model.getPrice(), model.getImageLarge());
+        var modelDto = new ModelFullDto(model.getId(), model.getCategory(), model.getName(), model.getDescription(), model.getAge().getValue(), model.getGender().getValue(), model.getVendor().getName(), model.getPrice(), model.getImageLarge());
         var products = model.getProducts();
         List<ProductDto> productsDto;
         if (model.getCategory() == Category.SKI) {
@@ -38,32 +31,8 @@ public class ModelFullDto {
         return modelDto;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public short getCategory() {
-        return category;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public String getVendor() {
-        return vendor;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public byte[] getImage() {
-        return image;
     }
 
     public List<ProductDto> getProducts() {
@@ -74,33 +43,23 @@ public class ModelFullDto {
         this.products = products;
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setCategory(short category) {
-        this.category = category;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
     public void setDescription(String description) {
         this.description = description;
     }
 
-    public void setVendor(String vendor) {
-        this.vendor = vendor;
+    public String getAge() {
+        return age;
     }
 
-    public void setPrice(int price) {
-        this.price = price;
+    public void setAge(String age) {
+        this.age = age;
     }
 
-    public void setImage(byte[] image) {
-        this.image = image;
+    public String getGender() {
+        return gender;
     }
 
-
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
