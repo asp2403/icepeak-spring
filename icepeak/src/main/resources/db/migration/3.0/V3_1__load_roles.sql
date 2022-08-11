9dd761a77c4f1bab96e65fa205f2bb7b20f55768
Alexander Popov <as-popov@yandex.ru>
2022-08-11 10:50:00 +0300

    security: Миграции

---
+++ icepeak/src/main/resources/db/migration/3.0/V3_1__load_roles.sql
@@ -0,0 +1,3 @@
+insert into d_role(id_role, role_name) values (1, 'ROLE_ADMIN');
+insert into d_role(id_role, role_name) values (2, 'ROLE_MANAGER');
+insert into d_role(id_role, role_name) values (3, 'ROLE_CUSTOMER');
